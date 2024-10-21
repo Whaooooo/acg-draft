522a77e3f6ce4ddd23f54bdd4aea005c1e193d36
unprintable <unprintable123@gmail.com>
2024-10-21 14:33:09 +0800

    fix

--- src/Managers/CameraManager.ts
+++ src/Managers/CameraManager.ts
@@ -15,8 +15,8 @@ export class CameraManager {
             const camera = new THREE.PerspectiveCamera(
                 75,
                 window.innerWidth / window.innerHeight,
-                0.01,
-                1000
+                0.1,
+                100000
             );
 
             // Initial position and orientation
