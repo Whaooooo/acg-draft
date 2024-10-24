224306d37cb3e27a1c0e6785102b530c3d6f12c4
Zheou Zhou <3225329724@qq.com>
2024-10-24 23:41:30 +0800

    [Main_menu]

--- src/Configs/SoundPaths.ts
+++ src/Configs/SoundPaths.ts
@@ -5,7 +5,7 @@ export const SoundPaths = {
     f14engine: 'dcs/plane/F14_Engine_Rear_Close_New.wav',
     f14afterburner: 'dcs/plane/F14_Afterburner_Rear_Close.wav',
     wind: 'dcs/plane/PlaneWind.wav',
-    exs2engine: 'dcs/plane/Exs2.wav',
+    exs2engine: 'dcs/plane/ExS2.wav',
     missileshot: 'dcs/missile/rocket-loop-99748_cut.mp3',
     propeller: 'plane/engine.mp3', //https://github.com/NikLever/threejs-games-course
     explosion: 'plane/explosion-47821.mp3', //https://cdn.pixabay.com/download/audio/2022/03/10/audio_3c625ab051.mp3?filename=explosion-47821.mp3
